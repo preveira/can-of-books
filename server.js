08063c9926c06db08c160c1c16299915983d2c95
Cheeseman757 <bigcheese411@gmail.com>
2024-03-11 19:13:25 -0700

    server

--- server.js
+++ server.js
@@ -3,16 +3,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require("mongoose")
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+const DATABASE_URL=process.env.DATABASE_URL
 app.get('/test', (request, response) => {
 
   response.send('test request received')
 
 })
-
+mongoose.connect(DATABASE_URL).then( ()=> console.log("connected")) 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
